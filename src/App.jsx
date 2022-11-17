e7283d1df2bfd160252dcbd73da9615b8f4426eb
poolaprasanna <59352533+poolaprasanna@users.noreply.github.com>
2022-11-17 12:32:05 +0000

    just done some practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 function App() {
+  var name ="intergalactic alliance"
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className="container">
+    <article>
+      <img src="images/group.png" alt="team picture" />
+      <h1>Meet the <i style={{color:"SteelBlue"}}>StarGazers</i></h1>
+      <p>Members of an <b><i>{name}</i></b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+    </article>
+    <button onClick={()=>alert("button clicked")}>click Me</button>
+    </div>
   )
 }
 export default App
